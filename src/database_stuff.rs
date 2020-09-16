c2f9e49717171a7b105e2aac4a11bfd805e5a793
grode <dimitris23bp@gmail.com>
2020-09-16 20:38:03 +0300

    Minor fixes and function for delete of all data

--- src/database_stuff.rs
+++ src/database_stuff.rs
@@ -12,15 +12,15 @@ pub fn establish_connection() -> MysqlConnection {
 }
 
 use super::models::{Temperature, NewTemperature};
-// To access the table
-use crate::schema::temperatures::dsl::*;
+// Import columns, so I can select them when I get the data
 use crate::schema::temperatures::columns::{date_saved, date_of_forecast};
+// Import the table to insert
+use super::schema::temperatures;
+
 use crate::models::*;
 
 pub fn insert_temperature(conn: &MysqlConnection, new_temperatures: Vec<NewTemperature>) {
 
-    // Import the table
-    use super::schema::temperatures;
 
     // Get date from an index that is going to be inserted
     // Every index has the same date_saved, so it doesn't matter which one I get
@@ -37,16 +37,22 @@ pub fn insert_temperature(conn: &MysqlConnection, new_temperatures: Vec<NewTempe
     }
 }
 
+pub fn delete_all(connection: &MysqlConnection) {
+    diesel::delete(temperatures::table)
+        .execute(connection)
+        .expect("Error deleting posts");
+}
+
 pub fn get_from_date(connection: &MysqlConnection, date: String, type_of_date: Date) -> Vec<Temperature>{
 
     let temperatures_vec: Vec<Temperature>;
 
     match type_of_date {
-        Date::DateSaved => temperatures_vec = temperatures
+        Date::DateSaved => temperatures_vec = temperatures::table
             .filter(date_saved.eq(date))
             .load::<Temperature>(connection)
             .expect("Error loading temperatures from saved date"),
-        Date::DateOfForecast => temperatures_vec = temperatures
+        Date::DateOfForecast => temperatures_vec = temperatures::table
             .filter(date_of_forecast.eq(date))
             .load::<Temperature>(connection)
             .expect("Error loading temperatures from forecast's date"),
@@ -57,7 +63,7 @@ pub fn get_from_date(connection: &MysqlConnection, date: String, type_of_date: D
 
 fn no_data_for_date(connection: &MysqlConnection, date: String) -> bool{
 
-    let results: Vec<Temperature> = temperatures
+    let results: Vec<Temperature> = temperatures::table
         .filter(date_saved.eq(date))
         .load::<Temperature>(connection)
         .expect("Error loading temperatures from saved date");
@@ -78,32 +84,32 @@ pub fn add_dummy_data(connection: &MysqlConnection) {
 
     let dummy_data: Vec<NewTemperature> = vec![
         NewTemperature {
-            minimum: 0.0,
-            maximum: 0.0,
+            minimum: 25.3,
+            maximum: 31.4,
             date_of_forecast: String::from("DUMMY"),
             date_saved: String::from("1980-1-1")
         },
         NewTemperature {
-            minimum: 0.0,
-            maximum: 0.0,
+            minimum: 24.3,
+            maximum: 30.4,
             date_of_forecast: String::from("DUMMY"),
             date_saved: String::from("1980-1-2")
         },
         NewTemperature {
-            minimum: 0.0,
-            maximum: 0.0,
+            minimum: 25.7,
+            maximum: 32.0,
             date_of_forecast: String::from("DUMMY"),
             date_saved: String::from("1980-1-3")
         },
         NewTemperature {
-            minimum: 0.0,
-            maximum: 0.0,
+            minimum: 24.6,
+            maximum: 31.9,
             date_of_forecast: String::from("DUMMY"),
             date_saved: String::from("1980-1-4")
         },
         NewTemperature {
-            minimum: 0.0,
-            maximum: 0.0,
+            minimum: 25.0,
+            maximum: 31.7,
             date_of_forecast: String::from("DUMMY"),
             date_saved: String::from("1980-1-5")
         }
